a7fd44b513af73faac32936a8d3ece916df89bf2
Stanislav Jakuschevskij <Stas@2G.local>
2016-06-11 20:06:43 +0200

    updated server settings for openshift

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ var api     = require('./user-modules/api');
 var app = express();
 
 // conf
-app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); 
+
+// openshift/local server conf
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 // routing
 app.get('/api/location', api.search);
 
 // start app
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+app.listen(port, ipaddress, function () {
+    console.log('Server running on http://' + ipaddress + ':' + port);
 });
